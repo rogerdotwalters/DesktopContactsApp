cef2e5427db84d580456cf833314515fb57563aa
rogerdotwalters <rogerwalters3@gmail.com>
2024-11-20 14:57:24 -0600

    Add project files.

---
+++ DesktopContactsApp/ContactDetailsWIndow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using DesktopContactsApp.Classes;
+using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DesktopContactsApp {
+    /// <summary>
+    /// Interaction logic for ContactDetailsWIndow.xaml
+    /// </summary>
+    public partial class ContactDetailsWIndow : Window {
+
+        Contact contact;
+
+        public event EventHandler OnContactChanged;
+        public ContactDetailsWIndow(Contact contact) {
+            InitializeComponent();
+            Owner = Application.Current.MainWindow;
+            WindowStartupLocation = WindowStartupLocation.CenterOwner;
+            this.contact = contact;
+            tbName.Text = contact.Name;
+            tbEmail.Text = contact.Email;
+            tbPhone.Text = contact.Phone;
+        }
+
+        private void updateButton_Click(object sender, RoutedEventArgs e) {
+
+            contact.Name = tbName.Text;
+            contact.Email = tbEmail.Text;
+            contact.Phone = tbPhone.Text;
+
+            using (SQLiteConnection connection = new SQLiteConnection(App.databasePath)) {
+
+                connection.CreateTable<Contact>();
+                connection.Update(contact);
+            }
+
+            OnContactChanged?.Invoke(this, EventArgs.Empty);
+            Close();
+        }
+
+        private void deleteButton_Click(object sender, RoutedEventArgs e) {
+
+            using (SQLiteConnection connection = new SQLiteConnection(App.databasePath)) {
+
+                connection.CreateTable<Contact>();
+                connection.Delete(contact);
+            }
+
+            OnContactChanged?.Invoke(this, EventArgs.Empty);
+            Close();
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e) {
+
+            Close();
+        }
+    }
+}
---
+++ DesktopContactsApp/MainWindow.xaml.cs
@@ -0,0 +1,74 @@
+﻿using DesktopContactsApp.Classes;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DesktopContactsApp {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+
+        List<Contact> contacts;
+        public MainWindow() {
+            InitializeComponent();
+            contacts = new List<Contact>();
+            ReadDatabase();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e) {
+
+            NewContactWindow newContactWindow = new NewContactWindow();
+            newContactWindow.ShowDialog();
+
+            ReadDatabase();
+        }
+
+        void ReadDatabase() {
+
+            using (SQLite.SQLiteConnection conn = new SQLite.SQLiteConnection(App.databasePath)) {
+
+                conn.CreateTable<Contact>();
+                contacts = (conn.Table<Contact>().ToList()).OrderBy(c => c.Name).ToList();
+            }
+
+            if (contacts != null) {
+
+                contactsListView.ItemsSource = contacts;
+            }
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e) {
+
+            TextBox searchTextBox = sender as TextBox;
+
+            var filteredList = contacts.Where(c => c.Name.ToLower().Contains(searchTextBox.Text.ToLower())).ToList();
+
+            contactsListView.ItemsSource = filteredList;
+        }
+
+        private void contactsListView_SelectionChanged(object sender, SelectionChangedEventArgs e) {
+
+            Contact selectedContact = (Contact)contactsListView.SelectedItem;
+
+            if (selectedContact != null) {
+
+                ContactDetailsWIndow contactDetailsWindow = new ContactDetailsWIndow(selectedContact);
+                contactDetailsWindow.OnContactChanged += ContactDetailsWindow_OnContactChanged;
+                contactDetailsWindow.ShowDialog();
+            }
+        }
+
+        private void ContactDetailsWindow_OnContactChanged(object? sender, EventArgs e) {
+            
+            ReadDatabase();
+        }
+    }
+}
